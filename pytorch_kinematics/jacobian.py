Fix jacobian

pytorch_kinematics/jacobian.py
@@ -37,43 +37,25 @@ def calc_jacobian(serial_chain, th, tool=None):
             )
         cur_transform = tool.get_matrix()
 
-    cnt = 0
-    for f in reversed(serial_chain._serial_frames):
-        if f.joint.joint_type == "revolute":
-            cnt += 1
-            d = torch.stack(
-                [
-                    -cur_transform[:, 0, 0] * cur_transform[:, 1, 3]
-                    + cur_transform[:, 1, 0] * cur_transform[:, 0, 3],
-                    -cur_transform[:, 0, 1] * cur_transform[:, 1, 3]
-                    + cur_transform[:, 1, 1] * cur_transform[:, 0, 3],
-                    -cur_transform[:, 0, 2] * cur_transform[:, 1, 3]
-                    + cur_transform[:, 1, 2] * cur_transform[:, 0, 3],
-                ]
-            ).transpose(0, 1)
-            # d = torch.stack(
-            #     [
-            #         -cur_transform[:, 0, 0] * cur_transform[:, 2, 3]
-            #         + cur_transform[:, 1, 0] * cur_transform[:, 1, 3],
-            #         -cur_transform[:, 0, 2] * cur_transform[:, 0, 3]
-            #         + cur_transform[:, 1, 2] * cur_transform[:, 2, 3],
-            #         -cur_transform[:, 0, 1] * cur_transform[:, 1, 3]
-            #         + cur_transform[:, 1, 1] * cur_transform[:, 0, 3],
-            #     ]
-            # ).transpose(0, 1)
-            delta = cur_transform[:, 2, 0:3]
-            j_fl[:, :, -cnt] = torch.cat((d, delta), dim=-1)
-        elif f.joint.joint_type == "prismatic":
-            cnt += 1
-            j_fl[:, :3, -cnt] = f.joint.axis.repeat(N, 1) @ cur_transform[:, :3, :3]
-        cur_frame_transform = f.get_transform(th[:, -cnt].view(N, 1)).get_matrix()
-        cur_transform = cur_frame_transform @ cur_transform
+    final_pose = serial_chain.forward_kinematics(th, end_only=True).get_matrix()
+    final_pos = final_pose[:, :3, 3]
 
-    # currently j_fl is Jacobian in flange (end-effector) frame, convert to base/world frame
-    pose = serial_chain.forward_kinematics(th).get_matrix()
-    rotation = pose[:, :3, :3]
-    j_tr = torch.zeros((N, 6, 6), dtype=serial_chain.dtype, device=serial_chain.device)
-    j_tr[:, :3, :3] = rotation
-    j_tr[:, 3:, 3:] = rotation
-    j_w = j_tr @ j_fl
-    return j_w
+    cnt = 0
+    for f in serial_chain._serial_frames:
+        if f.joint.joint_type == "fixed":
+            tensor = th[:, 0].view(N, 1)
+        else:
+            tensor = th[:, cnt].view(N, 1)
+        cur_frame_transform = f.get_transform(tensor).get_matrix()
+        cur_transform = cur_transform @ cur_frame_transform
+        if f.joint.joint_type == "revolute":
+            delta = final_pos - cur_transform[:, :3, 3]
+            angles = f.joint.axis.repeat(N) @ cur_transform[:, :3, :3]
+            position = torch.cross(angles, delta)
+            j_fl[:, :, cnt] = torch.cat((position, angles), dim=-1)
+            cnt += 1
+        elif f.joint.joint_type == "prismatic":
+            j_fl[:, :3, cnt] = f.joint.axis.repeat(N, 1) @ cur_transform[:, :3, :3]
+            cnt += 1
+
+    return j_fl